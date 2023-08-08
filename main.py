57307246c1369bf56d9a5857aa4faf672c601450
liguoqinjim <liguoqinjim23@gmail.com>
2023-08-08 15:24:58 +0800

    feat($): update

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import shutil
 
 def copy_record():
     # 复制文件到指定路径
-    filepath = "/Users/li/Workspace/github.com/sunset_notify/crawler/weibo/上海火烧云预测bot/7612166895.csv"
+    # filepath = "/Users/li/Workspace/github.com/sunset_notify/crawler/weibo/上海火烧云预测bot/7612166895.csv"
+    filepath = "crawler/weibo/上海火烧云预测bot/7612166895.csv"
     new_dir = "records"
 
     shutil.copy(filepath, new_dir)
